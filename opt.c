5eb61b9dc2a8c90f95ca7ec59a7d09b005ae8917
ard94 <adib.ridhwan@hotmail.com>
2017-07-11 13:04:52 -0400

    Add files via upload

--- opt.c
+++ opt.c
@@ -12,6 +12,16 @@ extern int debug;
 
 extern struct frame *coremap;
 
+extern char *tracefile;
+
+typedef struct v_list{
+	addr_t vaddr;
+	struct v_list *next;
+}v_node; 
+
+struct v_list* head;
+struct v_list* tail;
+
 /* Page to evict is chosen using the optimal (aka MIN) algorithm. 
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
@@ -34,6 +44,36 @@ void opt_ref(pgtbl_entry_t *p) {
  * replacement algorithm.
  */
 void opt_init() {
+	FILE *file = stdin;
+	char buf[256];
+	addr_t vaddr = 0;
+	char type;
+	
+	if((file = fopen(tracefile, "r")) == NULL) {
+			perror("Error opening tracefile:");
+			exit(1);
+	}
+	head = NULL;
+	tail = NULL;
+	
+	while(fgets(buf, 256, file) != NULL) {
+		if(buf[0] != '=') {
+			sscanf(buf, "%c %lx", &type, &vaddr);
+			if(debug)  {
+				printf("%c %lx\n", type, vaddr);
+			}
+			printf("%lx\n", vaddr);
+			//access_mem(type, vaddr);
+			v_node = malloc(sizeof(v_node));
+			v_node->vaddr = vaddr;
+			v_node->next = NULL;
+			
+		} else {
+			continue;
+		}
+
+	}
+	
 
 }
 
